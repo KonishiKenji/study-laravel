5be84c7c0971e5f4f17c8715c281832f6660c2d7
k_konishi <konishi.b0engineer@gmail.com>
2019-08-11 22:58:09 +0900

    不要なコメント行の削除

--- laravel-library/app/Http/Controllers/LibraryController.php
+++ laravel-library/app/Http/Controllers/LibraryController.php
@@ -7,9 +7,7 @@ use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Input;
 
-//[add]20181021 次の行を追加
 use App\Library;
-//[/add]20181021 次の行を追加
 
 class LibraryController extends Controller
 {
@@ -24,26 +22,22 @@ class LibraryController extends Controller
         //
     }
 
-    // public function index(Request $request)
     public function index()
     {
-      // $test = "test";
-      // return view('library-old/libraryHome');
       return view('library/libraryHome');
 
     }
 
     public function borrowingList()
     {
-      // return view('library-old/libraryBorrowingList');
       return view('library/libraryBorrowingList');
 
     }
 
     public function borrowHistory()
     {
-      // return view('library-old/libraryBorrowHistory');
       return view('library/libraryBorrowHistory');
+
     }
 
 }
--- laravel-library/routes/web.php
+++ laravel-library/routes/web.php
@@ -11,53 +11,29 @@
 |
 */
 // ======================================================================
-// For LibraryController
-// Route::get('/libraryHome', 'LibraryController@index');
-// Route::get('/libraryBorrowingList', 'LibraryController@borrowingList');
-// Route::get('/libraryBorrowHistory', 'LibraryController@borrowHistory');
-
-// ======================================================================
-// For BooksController - Rent_logsController
-// Route::get('/libraryHome', 'BooksController@index');
-// Route::get('/libraryBorrowingList', 'BooksController@borrowingList');
-// Route::get('/libraryBookSignUp', 'BooksController@bookSignUp');
-Route::post('library/bookRegister', 'BooksController@bookRegister');
-// Route::get('/libraryBookEdit/{id}', 'BooksController@bookEdit');
-Route::post('library/bookUpdate', 'BooksController@bookUpdate');
-Route::post('library/bookDelete/{id}', 'BooksController@bookDelete');
-
-// Route::get('/libraryBorrowHistory', 'Rent_logsController@index');
-
-// ======================================================================
-
-// Route::get('/', function () {
-//   // return view('welcome');
-// });
-
 Route::get('/', 'LoginController@index');
+Route::get('/home', 'HomeController@index')->name('home');
 
-Route::post('/library/bookBorrow', 'BooksController@bookBorrow');
-Route::post('/library/bookReturn', 'BooksController@bookReturn');
-
+// ======================================================================
 // Auth::routes();
 // Email Verification機能を使えるようにするため下記に変更
 Auth::routes(['verify' => true]);
 
+// For BooksController - Rent_logsController
+// 本登録ユーザーだけ表示できるページ
 Route::middleware('verified')->group(function() {
-    // 本登録ユーザーだけ表示できるページ
     // For BooksController - Rent_logsController
     Route::get('/libraryHome', 'BooksController@index');
     Route::get('/libraryBorrowingList', 'BooksController@borrowingList');
     Route::get('/libraryBookSignUp', 'BooksController@bookSignUp');
-    // Route::post('library/bookRegister', 'BooksController@bookRegister');
     Route::get('/libraryBookEdit/{id}', 'BooksController@bookEdit');
-    // Route::post('library/bookUpdate', 'BooksController@bookUpdate');
-    // Route::post('library/bookDelete/{id}', 'BooksController@bookDelete');
-
     Route::get('/libraryBorrowHistory', 'Rent_logsController@index');
-
-
+    
+    Route::post('/library/bookBorrow', 'BooksController@bookBorrow');
+    Route::post('/library/bookReturn', 'BooksController@bookReturn');
+    Route::post('library/bookRegister', 'BooksController@bookRegister');
+    Route::post('library/bookUpdate', 'BooksController@bookUpdate');
+    Route::post('library/bookDelete/{id}', 'BooksController@bookDelete');
+    
 });
 
-
-Route::get('/home', 'HomeController@index')->name('home');
